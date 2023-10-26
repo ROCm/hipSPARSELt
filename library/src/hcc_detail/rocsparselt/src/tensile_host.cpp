0d848a584b0fe05933ab20cd752726715d2ecbe2
AravindanC <aravindan.cheruvally@amd.com>
2023-10-25 23:19:03 -0700

    ASAN build excluding additional files for packaging
    [ROCm/hipSPARSELt commit: c43afa490ec433ad145112ba206e98928ba08831]

--- library/src/hcc_detail/rocsparselt/src/tensile_host.cpp
+++ library/src/hcc_detail/rocsparselt/src/tensile_host.cpp
@@ -534,6 +534,8 @@ namespace
                 // Find the location of the libraries
                 if(TestPath(path + "/../Tensile/library"))
                     path += "/../Tensile/library";
+                else if(TestPath(path + "../hipsparselt/library"))
+                    path += "../hipsparselt/library";
                 else
                     path += "/hipsparselt/library";
 
